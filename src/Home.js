ae0d91e858e64fd3b1d4128700b8863a4babd4e7
Sam <samuel.burns.25@gmail.com>
2018-04-25 16:58:21 -0400

    initial layout

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,22 @@ import React, { Component } from "react";
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h2>HELLOO</h2>
+      <div className="f">
+        <div className="header">
+          <img className="my-face" src={require('./images/sb1.png')} />
+        </div>
+
+        <div className="contact-holder">
+          <h1 className="large-6 medium-6 small-12 text-center columns name">Sam Burns</h1>
+
+          <div className="large-6 medium-6 small-12 text-center columns">
+            <p className="number">607-592-9072</p>
+            <p className="email">samuel.burns.25@gmail.com</p>
+            <a className="linkedin" href="https://www.linkedin.com/in/samuel-burns/" target="_blank">LinkedIn</a><br /><br />
+            <a className="github" href="https://github.com/BurnsBlink" target="_blank">GitHub</a>
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -8,19 +8,10 @@ class Main extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="row">
-          <div className="small-2 large-3 columns">
-            <ul className="header">
-              <li><NavLink to="/">Home</NavLink></li>
-              <li><NavLink to="/categories">Categories</NavLink></li>
-              <li><NavLink to="/personal">Personal</NavLink></li>
-            </ul>
-          </div>
-          <div className="small-10 large-9 columns text-center content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/categories" component={Categories}/>
-            <Route path="/personal" component={Personal}/>
-          </div>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/categories" component={Categories}/>
+          <Route path="/personal" component={Personal}/>
         </div>
       </HashRouter>
     );
