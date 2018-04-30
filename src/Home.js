d146e29c39de412c66a43eb235220bd348f6ac43
Sam <samuel.burns.25@gmail.com>
2018-04-30 15:17:11 -0400

    testing pdf viewer

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Document } from 'react-pdf';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="f">
         <div className="header">
-          <img className="my-face" src={require('./images/sb1.png')} />
+          <img className="my-face" src={require('./images/sb1.png')} alt=""/>
         </div>
 
         <div className="contact-holder">
@@ -14,10 +15,12 @@ class Home extends React.Component {
           <div className="large-6 medium-6 small-12 text-center columns">
             <p className="number">607-592-9072</p>
             <p className="email">samuel.burns.25@gmail.com</p>
-            <a className="linkedin" href="https://www.linkedin.com/in/samuel-burns/" target="_blank">LinkedIn</a><br /><br />
-            <a className="github" href="https://github.com/BurnsBlink" target="_blank">GitHub</a>
+            <a className="linkedin" href="https://www.linkedin.com/in/samuel-burns/" target="_blank" rel="noopener noreferrer">LinkedIn</a><br /><br />
+            <a className="github" href="https://github.com/BurnsBlink" target="_blank" rel="noopener noreferrer">GitHub</a>
           </div>
         </div>
+
+        <Document file="resume.pdf" />
 
       </div>
     );
